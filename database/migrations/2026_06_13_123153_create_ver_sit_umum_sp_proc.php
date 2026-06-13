4182c73464e9cef560a62fc6843c03c037c429ca
agung ap <agungaprianto96@gmail.com>
2026-06-14 01:34:58 +0700

    Reformat ver_sit_umum_sp migration SQL
    Normalize indentation inside the CREATE PROCEDURE string and use
    single quotes for the DROP statement in down() to match project style

--- database/migrations/2026_06_13_123153_create_ver_sit_umum_sp_proc.php
+++ database/migrations/2026_06_13_123153_create_ver_sit_umum_sp_proc.php
@@ -11,38 +11,38 @@ return new class extends Migration
     public function up(): void
     {
         DB::unprepared("CREATE OR ALTER PROCEDURE [dbo].[ver_sit_umum_sp]
-@kode_dokter as int,
-@kode_bagian as int,
-@biaya_dr1 as int,
-@id_jadwal as int
-
-as
--- dokter IGD
-if (@kode_bagian='020101')
-BEGIN
-insert into fee_dr_sitting_temp(kode_dr,kode_bagian,tgl_praktek,nama_tindakan,jumlah,id_jadwal,kode_jadwal)
-select kode_dokter,'020101' as kode_bagian,tgl_praktek,'Sitting Fee' as nama_tindakan,@biaya_dr1 as jumlah,id_jadwal,kode_jadwal
-from tc_jadwal_dokter where @kode_dokter=@kode_dokter and id_jadwal=@id_jadwal;
-update tc_jadwal_dokter set flag_ver=1 where kode_dokter=@kode_dokter and id_jadwal=@id_jadwal;
-END
-
--- dokter poli umum
-if (@kode_bagian='012201')
-BEGIN
-insert into fee_dr_sitting_temp(kode_dr,kode_bagian,tgl_praktek,nama_tindakan,jumlah,id_jadwal,kode_jadwal)
-select kode_dokter,'012201' as kode_bagian,tgl_praktek,'Sitting Fee' as nama_tindakan,@biaya_dr1 as jumlah,id_jadwal,kode_jadwal
-from tc_jadwal_dokter where @kode_dokter=@kode_dokter and id_jadwal=@id_jadwal;
-update tc_jadwal_dokter set flag_ver=1 where kode_dokter=@kode_dokter and id_jadwal=@id_jadwal;
-END
-
--- dokter bangsal
-if (@kode_bagian='030001')
-BEGIN
-insert into fee_dr_sitting_temp(kode_dr,kode_bagian,tgl_praktek,nama_tindakan,jumlah,id_jadwal,kode_jadwal)
-select kode_dokter,'030001' as kode_bagian,tgl_praktek,'Sitting Fee' as nama_tindakan,@biaya_dr1 as jumlah,id_jadwal,kode_jadwal
-from tc_jadwal_dokter where @kode_dokter=@kode_dokter and id_jadwal=@id_jadwal;
-update tc_jadwal_dokter set flag_ver=1 where kode_dokter=@kode_dokter and id_jadwal=@id_jadwal;
-END");
+            @kode_dokter as int,
+            @kode_bagian as int,
+            @biaya_dr1 as int,
+            @id_jadwal as int
+            
+            as
+            -- dokter IGD
+            if (@kode_bagian='020101')
+            BEGIN
+                insert into fee_dr_sitting_temp(kode_dr,kode_bagian,tgl_praktek,nama_tindakan,jumlah,id_jadwal,kode_jadwal)
+                select kode_dokter,'020101' as kode_bagian,tgl_praktek,'Sitting Fee' as nama_tindakan,@biaya_dr1 as jumlah,id_jadwal,kode_jadwal
+                from tc_jadwal_dokter where @kode_dokter=@kode_dokter and id_jadwal=@id_jadwal;
+                update tc_jadwal_dokter set flag_ver=1 where kode_dokter=@kode_dokter and id_jadwal=@id_jadwal;
+            END
+            
+            -- dokter poli umum
+            if (@kode_bagian='012201')
+            BEGIN
+                insert into fee_dr_sitting_temp(kode_dr,kode_bagian,tgl_praktek,nama_tindakan,jumlah,id_jadwal,kode_jadwal)
+                select kode_dokter,'012201' as kode_bagian,tgl_praktek,'Sitting Fee' as nama_tindakan,@biaya_dr1 as jumlah,id_jadwal,kode_jadwal
+                from tc_jadwal_dokter where @kode_dokter=@kode_dokter and id_jadwal=@id_jadwal;
+                update tc_jadwal_dokter set flag_ver=1 where kode_dokter=@kode_dokter and id_jadwal=@id_jadwal;
+            END
+            
+            -- dokter bangsal
+            if (@kode_bagian='030001')
+            BEGIN
+                insert into fee_dr_sitting_temp(kode_dr,kode_bagian,tgl_praktek,nama_tindakan,jumlah,id_jadwal,kode_jadwal)
+                select kode_dokter,'030001' as kode_bagian,tgl_praktek,'Sitting Fee' as nama_tindakan,@biaya_dr1 as jumlah,id_jadwal,kode_jadwal
+                from tc_jadwal_dokter where @kode_dokter=@kode_dokter and id_jadwal=@id_jadwal;
+                update tc_jadwal_dokter set flag_ver=1 where kode_dokter=@kode_dokter and id_jadwal=@id_jadwal;
+            END");
     }
 
     /**
@@ -50,6 +50,6 @@ END");
      */
     public function down(): void
     {
-        DB::unprepared("DROP PROCEDURE IF EXISTS ver_sit_umum_sp");
+        DB::unprepared('DROP PROCEDURE IF EXISTS ver_sit_umum_sp');
     }
 };
