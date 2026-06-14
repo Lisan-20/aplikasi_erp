25658c8e6fb32d1128e060f5a8a4b756645ec34f
agung ap <agungaprianto96@gmail.com>
2026-06-14 11:56:58 +0700

    build: add Prettier code formatter with ESLint integration
    Add Prettier as a JS/JSX code formatter alongside ESLint:
    - Install prettier and eslint-config-prettier dev dependencies
    - Configure Prettier with standard defaults (no semi, single quotes, 2-space indent)
    - Add .prettierignore for common exclusion patterns
    - Add format and format:check npm scripts
    - Integrate with ESLint flat config via eslint-config-prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from '@eslint/js';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
     js.configs.recommended,
@@ -44,4 +45,5 @@ export default [
             'react/prop-types': 'off',
         },
     },
+    eslintConfigPrettier,
 ];
